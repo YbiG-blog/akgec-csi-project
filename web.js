389a9c0c64545ae897babf52f9c7a43e1a3f65d1
yash-blog <yashvardhan7906@gmail.com>
2022-01-03 00:59:53 +0530

    initial commit

---
+++ web.js
@@ -0,0 +1,13 @@
+function toggle()
+{
+    let para=document.getElementById('para');
+    let btn=document.getElementById('btn');
+    if(para.style.display!='none')
+    {
+        para.style.display='none';  
+    }
+    else
+    {
+        para.style.display='block';
+    }
+}
